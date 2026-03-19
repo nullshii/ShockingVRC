0a162215d8460d8c6be8857489543577d635c502
null <null>
2026-03-19 13:33:48 +0400

    Rough topbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
+import TopBar from './components/TopBar.vue';
+
 </script>
 
 <template>
-  <main>
+  <main class="flex flex-col h-screen w-full bg-bg text-text p-2">
+    <TopBar />
+    <div class="border-2 border-separator">
+      CENTRAL_CONTENT
+    </div>
+    <div class="flex flex-row border-2 border-separator">
+      BOTTOM_BAR
+    </div>
   </main>
 </template>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,29 @@
+<!-- TODO: Add fonts -->
+<!-- TODO: Add lines to drag area -->
+<template>
+    <div class="flex flex-row border-2 border-separator justify-between items-center">
+        <div
+            class="flex-row items-center border-2 border-separator justify-center min-w-8 min-h-8 flex overflow-hidden">
+            <img src="../assets/logo.svg" alt="logo" />
+        </div>
+        <div class="w-full h-full flex flex-row  border-2 border-separator items-center justify-between mx-2">
+            <div class="flex flex-row items-center justify-center gap-2">
+                <p class="text-text">
+                    Shocking VRC
+                </p>
+                <p class="text-text">
+                    //
+                </p>
+                <p class="text-accent underline">
+                    v 0.0.0
+                </p>
+            </div>
+            <img src="../assets/github.svg" alt="github" />
+        </div>
+        <div class="flex flex-row items-center border-2 border-separator justify-center gap-5 h-full">
+            <img class="min-w-5 min-h-5" src="../assets/controls/minimize.svg" alt="github" />
+            <img class="min-w-5 min-h-5" src="../assets/controls/maximize.svg" alt="github" />
+            <img class="min-w-5 min-h-5" src="../assets/controls/close.svg" alt="github" />
+        </div>
+    </div>
+</template>
