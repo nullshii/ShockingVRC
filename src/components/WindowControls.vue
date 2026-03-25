237ebb01ddf67e78ca0e4b89eb4eefdb975b62e0
null <null>
2026-03-25 19:03:17 +0400

    Added window control icon animation

--- src/components/WindowControls.vue
+++ src/components/WindowControls.vue
@@ -1,4 +1,9 @@
 <script lang="ts" setup>
+import CloseIcon from '@/assets/controls/close.svg?component';
+import MaximizeIcon from '@/assets/controls/maximize.svg?component';
+import MinimizeIcon from '@/assets/controls/minimize.svg?component';
+
+
 import { getCurrentWindow } from '@tauri-apps/api/window';
 
 const appWindow = getCurrentWindow();
@@ -19,13 +24,16 @@ const close = async () => {
 <template>
 	<div class="flex flex-row items-center ring-2 ring-separator justify-center gap-5 px-2 h-full">
 		<button tabindex="-1" @click="minimize">
-			<img class="drag-none min-w-5 min-h-5" src="../assets/controls/minimize.svg" alt="minimize" />
+			<component :is="MinimizeIcon"
+				class="drag-none min-w-5 min-h-5 transition-all duration-150 text-text hover:text-base-power" />
 		</button>
 		<button tabindex="-1" @click="toggleMaximize">
-			<img class="drag-none min-w-5 min-h-5" src="../assets/controls/maximize.svg" alt="maximize" />
+			<component :is="MaximizeIcon"
+				class="drag-none min-w-5 min-h-5 transition-all duration-150 text-text hover:text-over-power" />
 		</button>
 		<button tabindex="-1" @click="close">
-			<img class="drag-none min-w-5 min-h-5" src="../assets/controls/close.svg" alt="close" />
+			<component :is="CloseIcon"
+				class="drag-none min-w-5 min-h-5 transition-all duration-150 text-text hover:text-limit-power" />
 		</button>
 	</div>
 </template>
