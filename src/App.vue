8fbcfb4c6f13f0ae58e5fdc8b56a68956c2b1db8
null <null>
2026-03-25 18:57:51 +0400

    Added battery meter

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import BottomBar from './components/BottomBar.vue';
+import PowerSide from './components/PowerSide.vue';
 import TopBar from './components/TopBar.vue';
 
 </script>
@@ -7,7 +8,11 @@ import TopBar from './components/TopBar.vue';
 <template>
 	<main class="flex flex-col h-screen w-full bg-bg text-text border-2 border-separator">
 		<TopBar />
-		<div class="h-full">
+		<div class="h-full flex flex-row">
+			<PowerSide />
+			<div class="flex w-full">
+
+			</div>
 		</div>
 		<BottomBar />
 	</main>
---
+++ src/components/PowerSide.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import BatteryBar from './BatteryBar.vue';
+import PowerMeter from './PowerMeter.vue';
+
+</script>
+
+<template>
+	<div class="flex flex-col border-r-2 border-separator">
+		<BatteryBar :level="60" :is-charging=false />
+		<div class="flex flex-row h-full">
+			<PowerMeter />
+			<PowerMeter />
+		</div>
+	</div>
+</template>
